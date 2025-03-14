5cf3380d6f5581df1b232dd7c7da6aab3fd8b22b
nthiesse <129333526+nthiesse@users.noreply.github.com>
2025-03-14 13:21:12 -0400

    Delete Homework-2 directory

--- Homework-2/flask_app/static/main/js/main.js
+++
@@ -1,7 +0,0 @@
-// feedback button 
-const feedbackbutton = document.getElementById("feedbackbutton");
-const form = document.getElementById("feedbackform");
-// when the feedback button is pressed, have the form appear 
-feedbackbutton.addEventListener("click", () => {
-    form.style.display = form.style.display === 'none' || form.style.display === '' ? 'block' : 'none';
-});
